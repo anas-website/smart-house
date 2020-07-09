23f72d9ed2dcd999718edec9f4f3c196a111f717
anas <anas3506606@gmail.com>
2020-07-10 02:20:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import AddRoom from "./component/AddRoom";
 import Home from "./component/Home";
