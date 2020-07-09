2c70f99127efe1f900c9f587e039e2c68b69208b
anas <anas3506606@gmail.com>
2020-07-10 02:03:58 +0300

    Updates

---
+++ precache-manifest.7c47e03d04fc59836325edc77b8d8b32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31e6ad439eb3bfcd595286706ef14fa",
+    "url": "/smart-house/index.html"
+  },
+  {
+    "revision": "7709dbcd0485611bf86b",
+    "url": "/smart-house/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "34738be88d60fb5d1f2d",
+    "url": "/smart-house/static/css/main.8fedd26b.chunk.css"
+  },
+  {
+    "revision": "7709dbcd0485611bf86b",
+    "url": "/smart-house/static/js/2.257d9ed7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smart-house/static/js/2.257d9ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34738be88d60fb5d1f2d",
+    "url": "/smart-house/static/js/main.27ed96e2.chunk.js"
+  },
+  {
+    "revision": "e50620ee363c836d7f01",
+    "url": "/smart-house/static/js/runtime-main.6b43a7f7.js"
+  },
+  {
+    "revision": "f479f8c22875a98e3175af37f8f75d12",
+    "url": "/smart-house/static/media/inita-middle-header-01-1024x861.f479f8c2.png"
+  }
+]);
